5be32e7c8b02fcc5286325a1c18809f0d2d590d5
Solomon <solomonmeresa8@gmail.com>
2021-03-18 11:58:55 -0700

    add lyrics

--- server/server.js
+++ server/server.js
@@ -2,42 +2,46 @@ const express = require('express');
 const SpotifyWebApi = require('spotify-web-api-node')
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const lyricsFinder = require('lyrics-finder')
+require('dotenv').config()
 
 const app =  express();
 app.use(cors())
-app.use(bodyParser.json())
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 const PORT = 3001;
 
 app.post('/refresh',(req ,res) => {
   const refreshToken = req.body.refreshToken;
   const spotifyApi = new SpotifyWebApi({
-    redirectUri: 'http://localhost:3000',
-    clientId:'8a95e8689cd9491fbdc8bb6da35b2ea6',
-    clientSecret:'e465306ce94f4fd4a06d6b4f07abbb26',
-    refreshToken
-})
+        redirectUri: process.env.REDIRECT_URI,
+        clientId:process.env.CLIENT_ID ,
+        clientSecret:process.env.CLIENT_SECRET,
+        refreshToken
+    })
 
-// clientId, clientSecret and refreshToken has been set on the api object previous to this call.
-spotifyApi
-.refreshAccessToken()
-.then(data => {
-      res.json({
-          accessToken: data.body.accessToken,
-          expiresIn:data.body.expiresIn
-      })
-    })
-.catch(()=>{
-        res.sendStatus(400)
-    })
+    // clientId, clientSecret and refreshToken has been set on the api object previous to this call.
+    spotifyApi
+    .refreshAccessToken()
+    .then(data => {
+        res.json({
+            accessToken: data.body.accessToken,
+            expiresIn:data.body.expiresIn
+        })
+        })
+    .catch(()=>{
+            res.sendStatus(400)
+        })
   
 })
 
 app.post('/login', (req, res) => {
     const code = req.body.code;
     const spotifyApi = new SpotifyWebApi({
-        redirectUri: 'http://localhost:3000',
-        clientId:'8a95e8689cd9491fbdc8bb6da35b2ea6',
-        clientSecret:'e465306ce94f4fd4a06d6b4f07abbb26'
+        redirectUri: process.env.REDIRECT_URI,
+        clientId:process.env.CLIENT_ID ,
+        clientSecret:process.env.CLIENT_SECRET
     })
     spotifyApi.authorizationCodeGrant(code)
     .then(data => {
@@ -51,6 +55,11 @@ app.post('/login', (req, res) => {
         console.log(err);
         res.sendStaus(400)
     })
+})
+
+app.get('/lyrics',async (req, res)=>{
+    const lyrics =  (await lyricsFinder(req.query.track, req.query.track))|| 'No Lyrics Found'
+    res.json({lyrics})
 })
 
 app.listen(PORT, ()=>{
